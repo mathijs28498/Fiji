220e439335fd2ab6412040ddad2888bb1349760d
Mathijs Frank <33691132+mathijs28498@users.noreply.github.com>
2022-11-13 16:10:08 +0000

    Changed render_pass to pipeline names

--- src/public/objects/obj_2d/text.rs
+++ src/public/objects/obj_2d/text.rs
@@ -1,12 +1,37 @@
+use nalgebra_glm::{Vec2, Vec4};
+
+use crate::public::objects::DEFAULT_COLOR;
+
+use super::DEFAULT_POSITION_2D;
+
 #[derive(Clone, Debug)]
 pub struct Text {
     text: String,
+    color: Vec4,
+    position: Vec2,
 }
 
 impl Text {
     pub fn new_with_text(text: &str) -> Self {
         Self {
             text: text.to_string(),
+            color: DEFAULT_COLOR,
+            position: DEFAULT_POSITION_2D,
         }
+    }
+
+    pub fn with_text(mut self, text: &str) -> Self {
+        self.text = text.to_string();
+        Self
+    }
+
+    pub fn with_color(mut self, color: Vec4) -> Self {
+        self.color = color;
+        Self
+    }
+
+    pub fn with_text(mut self, position: Vec2) -> Self {
+        self.position = position;
+        Self
     }
 }
