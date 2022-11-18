58fc54ba90e68c007455813bb383ae85fa8a225f
Mathijs Frank <33691132+mathijs28498@users.noreply.github.com>
2022-11-18 23:03:01 +0000

    Added dt

--- examples/ui_example.rs
+++ examples/ui_example.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use fiji::{Background, Circle, Context, KeyCode, Rect, Text};
 use nalgebra_glm::{Vec2, Vec3, Vec4};
 
@@ -20,14 +22,18 @@ pub fn main() {
         if input.key_pressed(&KeyCode::Escape) {
             event_handler.exit();
         }
-        neg_timer -= 0.13;
-        context.camera_2d.position += Vec2::new(0.1, -0.1);
+
+        context.camera_2d.position += Vec2::new(50. * context.dt() as f32, 0.);
 
         context.rect(r.clone());
         context.text(
-            Text::new_with_text(&format!("{neg_timer:.2}"))
-                .with_position(Vec2::new(200., 100.))
-                .with_color(Vec4::new(0.9, 0.7, 0.2, 1.)),
+            Text::new_with_text(&format!(
+                "fps: {:.3} ({:.3}ms)",
+                context.fps(),
+                context.dt_micros() as f64 / 1000.
+            ))
+            .with_position(Vec2::new(200., 100.))
+            .with_color(Vec4::new(0.9, 0.7, 0.2, 1.)),
         );
         context.circle(
             Circle::new_default()
@@ -35,17 +41,25 @@ pub fn main() {
                 .with_position(Vec2::new(700., 400.)),
         );
 
-        context.ui_text(
-            Text::new_with_text(&format!("{neg_timer:.2}"))
-                .with_position(Vec2::new(400., 300.))
-                .with_color(Vec4::new(0.2, 0.7, 0.9, 1.)),
-        );
         context.ui_rect(r_2.clone());
         context.ui_circle(
             Circle::new_default()
                 .with_radius(50.)
                 .with_position(Vec2::new(600., 200.)),
         );
+
+        let text = Text::new_with_text(&format!(
+            "fps: {:.3} ({:.3}ms)",
+            context.fps(),
+            context.dt_micros() as f64 / 1000.
+        ));
+
+        for i in 0..5 {
+            context.ui_text(
+                text.clone()
+                    .with_position(Vec2::new(50., 50. + i as f32 * 20.)),
+            );
+        }
 
         context.render(event_handler);
     });
--- src/public/context.rs
+++ src/public/context.rs
@@ -1,4 +1,7 @@
+use std::time::SystemTime;
+
 use crate::{
+    input::fiji_events::FijiEventHandler,
     public::objects::{
         background::Background,
         camera::{camera_2d::Camera2D, camera_3d::Camera3D},
@@ -7,13 +10,15 @@ use crate::{
     },
     rendering::render_containers::{
         event_loop_container::EventLoopContainer, render_container::RenderContainer,
-    }, input::fiji_events::FijiEventHandler,
+    },
 };
 
 use super::objects::obj_2d::text::Text;
 
 pub struct Context {
     render_container: RenderContainer,
+    prev_time: SystemTime,
+    dt_nano: u128,
     pub camera_2d: Camera2D,
     pub camera_3d: Camera3D,
 }
@@ -22,6 +27,8 @@ impl Context {
     pub fn new(width: u32, height: u32) -> Self {
         Self {
             render_container: RenderContainer::new(width, height),
+            prev_time: SystemTime::now(),
+            dt_nano: 0,
             camera_2d: Camera2D::new_default(),
             camera_3d: Camera3D::new_default(),
         }
@@ -79,8 +86,34 @@ impl Context {
         self.render_container.event_loop()
     }
 
+    pub fn fps(&self) -> f32 {
+        1. / self.dt()
+    }
+
+    pub fn dt(&self) -> f32 {
+        self.dt_nano as f32 * 0.000000001
+    }
+
+    pub fn dt_f64(&self) -> f64 {
+        self.dt_nano as f64 * 0.000000001
+    }
+
+    pub fn dt_micros(&self) -> u128 {
+        self.dt_nano / 1_000
+    }
+
+    pub fn dt_millis(&self) -> u128 {
+        self.dt_nano / 1_000_000
+    }
+
     pub fn render(&mut self, fiji_event_handler: &mut FijiEventHandler) {
         self.render_container
             .render(fiji_event_handler, &self.camera_2d, &self.camera_3d);
+
+        let now = SystemTime::now();
+        if let Ok(duration) = now.duration_since(self.prev_time) {
+            self.dt_nano = duration.as_nanos();
+            self.prev_time = now;
+        }
     }
 }
